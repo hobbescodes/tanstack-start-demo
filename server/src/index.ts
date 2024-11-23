fe7ffe238c760022c39a05adfa062fabb92d099d
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-23 10:54:37 -0600

    feature: set up expenses route WIP

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
+import { expensesRoute } from "./routes/expenses";
+
 const app = new Hono();
 
 app.use("*", logger());
@@ -12,6 +14,8 @@ app.get("/", (c) => {
 app.get("/ping", (c) => {
   return c.json({ message: "pong" });
 });
+
+app.route("/api/expenses", expensesRoute);
 
 export default {
   port: process.env.PORT || 4000,
