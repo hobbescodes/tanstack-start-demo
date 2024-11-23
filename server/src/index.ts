cfe9cada41459db096927cec6d58f01aaa1a1861
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-23 10:48:54 -0600

    feature(server): add logging middleware, add ping endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
+app.use("*", logger());
+
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.get("/ping", (c) => {
+  return c.json({ message: "pong" });
 });
 
 export default {
