1be8f6adba034b6cd2e6f4c5f8b9dc484e239fd3
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-23 09:40:36 -0600

    feature: configure tailwindcss

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/start/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   vite: {
-    plugins: [tsconfigPaths()],
+    plugins: [tsconfigPaths(), tailwindcss()],
   },
 });
