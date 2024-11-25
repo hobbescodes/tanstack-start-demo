b76fba4cccfbfc5d443ebcb210e25f76765815e5
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-25 00:40:06 -0600

    refactor(home): update total expense to use fixed decimal place

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -53,7 +53,7 @@ const Home = () => {
           </CardHeader>
           <CardContent>
             <Skeleton isLoading={isLoading} skeletonClassName="max-w-16">
-              {data?.total ?? 0}
+              {(data?.total ?? 0).toFixed(2)}
             </Skeleton>
           </CardContent>
         </Card>
