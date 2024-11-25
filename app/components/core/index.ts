17d3944d517d39bfa85cd202218d07c1f3f33800
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-24 22:41:28 -0600

    chore: reorg exports

--- app/components/core/index.ts
+++ app/components/core/index.ts
@@ -1,6 +1,6 @@
 export { default as Button } from "./Button";
 export * from "./Card";
-export { default as Skeleton } from "./Skeleton";
 export { default as Input } from "./Input";
 export { default as Label } from "./Label";
+export { default as Skeleton } from "./Skeleton";
 export * from "./Table";
